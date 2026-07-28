c5425a2eb3090c049570277d64b9684b2d7d4733
JustAiden25 <aidenmoses25@gmail.com>
2026-07-28 21:26:20 +0200

    Change namespace import from Worlds to World

--- AgentSim.Core/Agents/IAgentBehaviour.cs
+++ AgentSim.Core/Agents/IAgentBehaviour.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AgentSim.Core.Utilities;
-using AgentSim.Core.Worlds;
+using AgentSim.Core.World;
 
 // Defines how an agent behaves on each simulation tick.
 // RandomWalkBehavior implements this for the MVP demo. Later, when
